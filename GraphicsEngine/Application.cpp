6c031209d69c70d4ed3bdeb7723d61175c6adb2f
ResidentUnleashed <31601762+ResidentUnleashed@users.noreply.github.com>
2018-05-15 10:35:29 +1000

    Application added

--- GraphicsEngine/Application.cpp
+++ GraphicsEngine/Application.cpp
@@ -2,18 +2,104 @@
 
 
 
-Application::Application()
-{
-}
-
-
 Application::~Application()
 {
 }
 
 int Application::Run(const char* title, int width, int height, bool fullscreen, const char* configFilePath)
 {
+	//Set up the sucessful check
+	auto successful = initialise(title, width, height, fullscreen);
+
+	if (!successful)
+	{
+		return -1;
+	}
+	else if (!onStartup())
+	{
+		shutdown();
+		return -2;
+	}
+
+	//Run game loop
+	gameLoop();
+	
+
+	//Shutdown
+	onShutdown();
+	shutdown();
+
 	return 0;
+}
+
+bool Application::initialise(const char* title, int width, int height, bool fullscreen)
+{
+	if (glfwInit() == false) /* If it hasn't initialised, return -1 */
+		return -1;
+
+	//Get the monitor and check if fullscreen
+	GLFWmonitor* monitor = nullptr;
+	if (fullscreen)
+	{
+		monitor = glfwGetPrimaryMonitor();
+	}
+
+	//Create the window
+	m_window = glfwCreateWindow(width, height, title, monitor, nullptr);
+
+	if (m_window == nullptr)
+	{
+		//Terminate process
+		glfwTerminate();
+		return false;
+	}
+
+	glfwMakeContextCurrent(m_window);
+
+	if (ogl_LoadFunctions() == ogl_LOAD_FAILED)
+	{
+		glfwDestroyWindow(m_window);
+		glfwTerminate();
+		return false;
+	}
+
+	//Initialise variables
+	m_windowWidth = width;
+	m_windowHeight = height;
+	m_previousFrameTime = m_applicationStartTime;
+
+	return true;
+}
+
+void Application::gameLoop()
+{
+	while (m_isRunning)
+	{
+		//Create deltatime by getting the time in nanoseconds and converting it to seconds
+		auto now = std::chrono::high_resolution_clock::now();
+		auto timeTaken = now - m_previousFrameTime;
+		m_dt = std::chrono::duration<float>(timeTaken).count;
+        m_previousFrameTime = now;
+
+		//Elapsed time is equal to current time - start time
+		auto elapsedTime = now - m_applicationStartTime;
+		m_elapsedTime = std::chrono::duration<float>(elapsedTime).count;
+
+		//Update and render
+		update();
+		render();
+
+		//Buffer and process
+		glfwSwapBuffers(m_window);
+		glfwPollEvents();
+	}
+}
+
+void Application::shutdown()
+{
+	//Destroy the window and terminate
+	glfwDestroyWindow(m_window);
+	glfwTerminate();
 }
 
 
--- GraphicsEngine/Application.h
+++ GraphicsEngine/Application.h
@@ -2,30 +2,65 @@
 #include <gl_core_4_4.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
-#include <Gizmos.h>
 #include <glm/glm.hpp>
 #include <glm/ext.hpp>
-using glm::vec3;
-using glm::vec4;
-using glm::mat4;
+#include <string>
+#include <chrono>
 
 class Application
 {
 public:
-	Application();
-	~Application();
 
-	void Run(const char* title, int width, int height);
+	//Deconstructor
+	virtual ~Application();
+
+	//Functions for children to call
+	virtual bool onStartup() = 0;
+	virtual void update() = 0;
+	virtual void render() = 0;
+	virtual void onShutdown() = 0;
+
+
+	//Run the application
+	int Run(const char* title, int width, int height, bool fullscreen, const char* configFilePath = "");
+
+	//Sets up the application by creating a window and also setting up children
+	bool initialise(const char* title, int width, int height, bool fullscreen);
+
+	//Creates the main game loop
+	void gameLoop();
+
+	//Shutdown the application window
+	void shutdown();
+
+
+
+	//Getters
+	GLFWwindow* getWindow() { return m_window; }
+	int getWidth() { return m_windowWidth; }
+	int getWindowHeight() { return m_windowHeight; }
+	bool getFullScreen() { return m_isFullScreen; }
+	bool getIsRunning() { return m_isRunning; }
+	float getDeltaTime() { return m_dt; }
+	float getElapsedTime() { return m_elapsedTime; }
+
+	//Setters
+	void SetIsRunning(bool value) { m_isRunning = value; }
+
+protected:
+
+
 
 private:
 
-	int createWindow(const char* title, int width, int height);
-	bool startup();
-	void shutdown();
-	void update(float deltaTime);
-	void draw();
-
-	GLFWwindow * window;
-
+	GLFWwindow* m_window; /* The applications window */
+	int m_windowWidth = 1280;
+	int m_windowHeight = 720;
+	bool m_isFullScreen = false;
+	bool m_isRunning = true;
+	float m_dt = 0.0f; /* DeltaTime */
+	float m_elapsedTime = 0.0f;
+	std::chrono::high_resolution_clock::time_point m_previousFrameTime; /* Previous frame time */
+	std::chrono::high_resolution_clock::time_point m_applicationStartTime; /* Start time for the application */
 };
 
